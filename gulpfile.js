cc879eff0f6e9f58d4f5e457b4668d4d8418b2d3
Nir Moav <nirm@wix.com>
2015-06-14 18:58:58 +0300

    adding lint, sass and watch tasks to the gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,33 @@
+// include Gulp
 var gulp = require('gulp');
+
+// include plugins
+var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
 var browserify = require('browserify');
 var babelify = require('babelify');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src('src/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
  
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('dist/css'));
+});
+
 gulp.task('build', function () {
   browserify({
     entries: 'src/index.js',
@@ -24,5 +46,12 @@ gulp.task('build', function () {
   .pipe(sourcemaps.write('./dist'))
   .pipe(gulp.dest('dist'));
 });
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', ['lint', 'build']);
+    gulp.watch('scss/**/*.scss', ['sass']);
+});
+
  
-gulp.task('default', ['build']);
+gulp.task('default', ['lint', 'sass', 'build']);
